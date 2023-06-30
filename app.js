cbea4f4fe8a466693d8b3d13d3ac0402bc7196a7
sujith surendran <sujithsurendran4433@gmail.com>
2023-06-30 14:21:51 +0530

    added kite connect

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const fs = require('fs');
+var KiteConnect = require('kiteconnect').KiteConnect;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -23,12 +25,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +39,58 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//Kite
+const token = require('./privateToken/token');
+var api_key = token.API_KEY,
+  secret = token.SECRET,
+  request_token = token.REQUEST_TOKEN,
+  access_token = '';
+//https://kite.zerodha.com/connect/login?v=3&api_key=909lcbtyglf6ks4o
+var options = {
+  api_key: api_key,
+  debug: false,
+};
+var InstrumentlabelINFY = 'NSE:NIFTY 50';
+
+myinstruments = [InstrumentlabelINFY];
+``;
+let kc = new KiteConnect(options);
+
+kc.setSessionExpiryHook(sessionHook);
+
+function sessionHook() {
+  console.log('User loggedout');
+}
+
+if (!access_token) {
+  kc.generateSession(request_token, secret)
+    .then(function (response) {
+      console.log('Response is online', response);
+      init();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+//Access code present
+else {
+  init();
+}
+function init() {
+  timeinterval = 1000;
+  var interval = setInterval(function () {
+    console.log(myinstruments);
+    kc.getLTP(myinstruments)
+      .then(function (response) {
+        console.log('response is done');
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log('this is an error', error);
+      });
+  }, timeinterval);
+}
+//
 
 module.exports = app;
